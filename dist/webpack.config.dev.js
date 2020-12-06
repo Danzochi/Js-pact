4e6f236e5a0ac478f7ee90a54a38b714ae766c2b
Sergey <s169112@gmail.com>
2020-12-06 19:07:23 +0300

    Update webpack.config.dev.js

--- dist/webpack.config.dev.js
+++ dist/webpack.config.dev.js
@@ -3,5 +3,11 @@
 var path = require('path');
 
 module.exports = {
-  context: path.resolve(__dirname, 'src')
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
