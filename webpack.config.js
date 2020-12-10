c1da78bcd09bf4f5318a13f12ddefdbbcfc8c030
Sergey <s169112@gmail.com>
2020-12-10 13:06:32 +0300

    up

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   entry: './index.js',
   output: {
     filename: filename('js'),
-    path: path.resolve(__dirname, 'dist/'),
+    path: path.resolve(__dirname, 'dist'),
   },
   resolve: {
     extensions: ['.js'], 
